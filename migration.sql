9d06487a5eb057b9e629648aec3d804df096b064
Jordan Leslie <this.codedev@gmail.com>
2017-10-25 15:59:58 -0500

    Update migration.sql to include pivot table

--- migration.sql
+++ migration.sql
@@ -1,7 +1,9 @@
 USE adlister_db;
 
+DROP TABLE IF EXISTS ads_categories;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -20,3 +22,19 @@ CREATE TABLE ads (
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE ads_categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ads_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (ads_id) REFERENCES ads(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
