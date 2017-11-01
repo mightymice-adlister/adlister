22a88211e1c2ea455c3c78abaf46adbac30bdcf0
Jordan Leslie <this.codedev@gmail.com>
2017-11-01 15:46:52 -0500

    Change position of delete edit buttons. Adds confirmation before delete

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,23 +1,7 @@
 "use strict";
 console.log("Im here");
-$( document ).ready(function(){
+$( document ).ready(function() {
   $(".dropdown-button").dropdown();
   $('select').material_select();
-  function confirmDelete(e) {
-    console.log("clicking");
-    $(e.target).innerHTML = `<button type="button"
-                    class="waves-effect waves-light red lighten-1 btn"
-                    name="deleteId"
-                    value="${ad.id}">Delete</button>`
-  }
-  $(".deleteBtn").on("click", confirmDelete);
-  $(".deleteBtn").on("click", function() {
-    console.log("clicked");
-  });
-  $(".deleteBtn").on("hover", function(){
-    console.log("hover")
-  });
-
-
-
-})();
+  $('.modal').modal();
+});
